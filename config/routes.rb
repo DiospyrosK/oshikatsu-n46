34b5d355b7d0251fce7bbde87ab9a09243f8652d
DiospyrosK <3.dsoul226@gmail.com>
2024-04-20 08:51:40 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :bookmarks, only: [:create, :destroy]
+  end
   resources :categories do
     get :children, on: :member
   end
