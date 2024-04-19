8ee5683edce929e226c872f0761d24f15e3c8633
DiospyrosK <3.dsoul226@gmail.com>
2024-04-20 05:22:33 +0900

    ルーティングとpostコントローラーの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.includes(:user).order('created_at DESC')
   end
@@ -17,9 +19,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    return unless current_user.id != @post.user_id
+    redirect_to root_path
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :explanation, :image, :category_id, member_ids: []).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
