adeae307c6397c6349b9bfff4b2b7df2f548c2ee
DiospyrosK <3.dsoul226@gmail.com>
2024-04-20 11:55:54 +0900

    非同期通信の設定の修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,18 +2,20 @@ class BookmarksController < ApplicationController
   before_action :set_post
 
   def create
-    bookmark = current_user.bookmarks.build(post_id: params[:post_id])
-    bookmark.save
-    respond_to do |format|
-      format.js
+    @bookmark = current_user.bookmarks.build(post_id: params[:post_id])
+    if @bookmark.save
+      respond_to do |format|
+        format.turbo_stream
+      end
     end
   end
 
   def destroy
-    bookmark = Bookmark.find_by(post_id: params[:post_id], user_id: current_user.id)
-    bookmark.destroy
-    respond_to do |format|
-      format.js
+    @bookmark = Bookmark.find_by(id: params[:id], post_id: params[:post_id], user_id: current_user.id)
+    if @bookmark.destroy
+      respond_to do |format|
+        format.turbo_stream
+      end
     end
   end
 
