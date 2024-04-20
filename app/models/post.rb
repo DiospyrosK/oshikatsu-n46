f43175884f0fd80f3021a8135c57869a5d5603a2
DiospyrosK <3.dsoul226@gmail.com>
2024-04-20 09:05:55 +0900

    bookmarkボタンの表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,8 @@ class Post < ApplicationRecord
   has_many :members, through: :post_members
   has_many :bookmarks
   has_one_attached :image
+
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user.id).exists?
+  end
 end
